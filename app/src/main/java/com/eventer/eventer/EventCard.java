0f17a5c7b518beab28e65549b1dfc78e6c354b57
David Choo <davidchoo16@gmail.com>
2015-08-09 01:52:49 +0800

    commented codes for image picker created placeholder tasks with task view layout Used 3rd party API for ripple effect created event card layout and allows programmatic adding of event card changed actionbaractivity to appcompatactivity

---
+++ app/src/main/java/com/eventer/eventer/EventCard.java
@@ -0,0 +1,147 @@
+package com.eventer.eventer;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.eventer.eventer.R;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by P1451298 on 8/8/2015.
+ */
+public class EventCard extends CardView {
+
+    private ImageView eventImage;
+    private TextView eventTitle;
+    private TextView eventTime;
+    private TextView eventLoc;
+    private TextView eventTaskCount;
+
+    public EventCard(Context context) {
+        super(context);
+        init();
+    }
+
+    public EventCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public EventCard(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        this.setRadius(16);
+        inflate(getContext(), R.layout.card_event, this);
+        this.eventImage = (ImageView)findViewById(R.id.event_image);
+        this.eventTitle = (TextView)findViewById(R.id.event_title);
+        this.eventTime = (TextView)findViewById(R.id.event_time_text);
+        this.eventLoc = (TextView)findViewById(R.id.event_location_text);
+        this.eventTaskCount = (TextView)findViewById(R.id.event_task_text);
+    }
+
+
+    public void setEventImage(int resourceId){
+        this.setPreventCornerOverlap(false);
+//        BitmapWorkerTask task = new BitmapWorkerTask(this.eventImage);
+//        task.execute(resourceId);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        bitmap = ImageHelper.getRoundedCornerBitmap(bitmap, (int)this.getRadius()*2);
+        this.eventImage.setImageBitmap(bitmap);
+    }
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle.setText(eventTitle);
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime.setText(eventTime);
+    }
+
+    public void setEventLoc(String eventLoc) {
+        this.eventLoc.setText(eventLoc);
+    }
+
+    public void setEventTaskCount(String eventTaskCount) {
+        this.eventTaskCount.setText(eventTaskCount);
+    }
+
+
+
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+    class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private int data = 0;
+
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage collected
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            data = params[0];
+            return BitmapFactory.decodeResource(getResources(), data);
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (imageViewReference != null && bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    ImageHelper.getRoundedCornerBitmap(bitmap, 16);
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/eventer/eventer/Main.java
+++ app/src/main/java/com/eventer/eventer/Main.java
@@ -1,35 +1,34 @@
 package com.eventer.eventer;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 
 
-public class Events extends ActionBarActivity
+public class Main extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
@@ -56,7 +55,7 @@ public class Events extends ActionBarActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
-
+    // method to handle screen navigations through drawer
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
@@ -64,11 +63,11 @@ public class Events extends ActionBarActivity
         FragmentManager fragmentManager = getSupportFragmentManager(); // For AppCompat use getSupportFragmentManager
         switch(position) {
             default:
-            case 0:
-                fragment = new Events.PlaceholderFragment().newInstance(1);
+            case 0: //Tasks
+                fragment = new TaskFragment().newInstance(1);
                 break;
-            case 1:
-                fragment = new Events.TaskFragment().newInstance(2);
+            case 1: //Events
+                fragment = new EventsFragment().newInstance(2);
                 break;
         }
         fragmentManager.beginTransaction()
@@ -138,53 +137,69 @@ public class Events extends ActionBarActivity
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        protected static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+        protected PlaceholderFragment setSectionNumber(int sectionNumber) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            this.setArguments(args);
+            return this;
+        }
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((Main) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class EventsFragment extends PlaceholderFragment {
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
+        public static EventsFragment newInstance(int sectionNumber) {
+            EventsFragment fragment = new EventsFragment();
+            return (EventsFragment)fragment.setSectionNumber(sectionNumber);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_events, container, false);
-            ((CardView)rootView.findViewById(R.id.card_view)).setPreventCornerOverlap(false);
-            ImageView image = (ImageView)rootView.findViewById(R.id.eventImage);
-            Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
-            bitmap = ImageHelper.getRoundedCornerBitmap(bitmap, 15);
-            image.setImageBitmap(bitmap);
-            return rootView;
-        }
+            EventCard edwardsWedding = (EventCard)rootView.findViewById(R.id.edwards_wedding_card);
+            edwardsWedding.setEventImage(R.drawable.event_image);
+            edwardsWedding.setEventTitle("Edward's Wedding asdf");
+            edwardsWedding.setEventTime("Sat, August 1, 7pm - 10pm asdf");
+            edwardsWedding.setEventLoc("Marina Bay Sands asdf");
+            edwardsWedding.setEventTaskCount("2 tasks : 2 todo asdf");
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((Events) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+            EventCard test = new EventCard(getActivity());
+            test.setEventImage(R.drawable.event_image);
+            test.setEventTitle("Edward's Wedding test");
+            test.setEventTime("Sat, August 1, 7pm - 10pm test");
+            test.setEventLoc("Marina Bay Sands test");
+            test.setEventTaskCount("2 tasks : 2 todo test");
+            // insert into main view
+            ViewGroup insertPoint = (ViewGroup) rootView.findViewById(R.id.event_list);
+            insertPoint.addView(test);
+
+            return rootView;
         }
     }
 
     /**
      * Task view fragment
      */
-    public static class TaskFragment extends Fragment {
+    public static class TaskFragment extends PlaceholderFragment {
         public static final int PICK_IMAGE = 1;
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        View rootView;
 
         /**
          * Returns a new instance of this fragment for the given section
@@ -192,35 +207,37 @@ public class Events extends ActionBarActivity
          */
         public static TaskFragment newInstance(int sectionNumber) {
             TaskFragment fragment = new TaskFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+            return (TaskFragment)fragment.setSectionNumber(sectionNumber);
         }
 
-        public TaskFragment() {
-        }
-        View rootView;
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             rootView = inflater.inflate(R.layout.fragment_tasks, container, false);
-            Button btn = (Button)rootView.findViewById(R.id.btn);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                    getIntent.setType("image/*");
 
-                    Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    pickIntent.setType("image/*");
+            LayoutInflater vi = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = vi.inflate(R.layout.task_item_view, null);
 
-                    Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+// fill in any details dynamically here
+            TextView textView = (TextView) v.findViewById(R.id.task_title);
+            textView.setText("New Task here");
 
-                    startActivityForResult(chooserIntent, PICK_IMAGE);
-                }
-            });
+// insert into main view
+            ViewGroup insertPoint = (ViewGroup) rootView.findViewById(R.id.task_list);
+            insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//            Button btn = (Button)rootView.findViewById(R.id.btn);
+//            btn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                    pickIntent.setType("image/*");
+//
+//                    Intent chooserIntent = Intent.createChooser(pickIntent, "Select Image");
+//
+//                    startActivityForResult(chooserIntent, PICK_IMAGE);
+//                }
+//            });
             return rootView;
         }
 
@@ -237,16 +254,10 @@ public class Events extends ActionBarActivity
                     InputStream is = context.getContentResolver().openInputStream(data.getData());
                     Bitmap bm = BitmapFactory.decodeStream(is);
                     ((ImageView)rootView.findViewById(R.id.img)).setImageBitmap(bm);
-                }catch(Exception e){}
-                //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
+                } catch(Exception e) {
+                    Toast.makeText(getActivity(), "Image not found", Toast.LENGTH_SHORT);
+                }
             }
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((Events) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 }
